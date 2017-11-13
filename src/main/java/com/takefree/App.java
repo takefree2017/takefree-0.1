5a185b89cecbd4d785c5e89d37b988a86186cf54
huojushen <zzy96@sina.com>
2017-11-13 15:46:34 +0800

    add category info

--- src/main/java/com/takefree/App.java
+++ src/main/java/com/takefree/App.java
@@ -3,9 +3,11 @@ package com.takefree;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.takefree.dao")//告诉mapper所在的包名
+@ComponentScan("com.takefree.*")
 public class App {
     public static void main( String[] args )
     {
