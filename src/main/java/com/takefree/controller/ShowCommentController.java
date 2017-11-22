f99cdb532966d561ec9e854ab353ff691c3ee0e1
gaoxiang <overgx_910@163.com>
2017-11-22 18:46:04 +0800

    add empty controller

---
+++ src/main/java/com/takefree/controller/ShowCommentController.java
@@ -0,0 +1,11 @@
+package com.takefree.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by gaoxiang on 2017/11/4.
+ */
+@Controller
+public class ShowCommentController {
+
+}
