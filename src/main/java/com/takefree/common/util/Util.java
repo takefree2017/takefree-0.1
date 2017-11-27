9fb25ada15e30f18bbddb370ce962d15cbdd330d
gaoxiang <overgx_910@163.com>
2017-11-27 17:30:49 +0800

    增加密码加密算法

--- src/main/java/com/takefree/common/util/Util.java
+++ src/main/java/com/takefree/common/util/Util.java
@@ -1,5 +1,8 @@
 package com.takefree.common.util;
 
+import com.xiaoleilu.hutool.crypto.SecureUtil;
+
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
@@ -13,5 +16,15 @@ public class Util {
         Pattern p = Pattern.compile(regExp);
         Matcher m = p.matcher(str);
         return m.matches();
+    }
+
+    public static String encryptPassword(String password){
+        return SecureUtil.md5(SecureUtil.md5(password));
+    }
+
+    public static String generateSmsCode(){
+        Random random = new Random(System.currentTimeMillis());
+        int code = random.nextInt(899999) + 100000;
+        return String.valueOf(code);
     }
 }
