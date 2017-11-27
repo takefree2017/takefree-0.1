ae48c9d49a5a07d868a7c4221cde4c0bd953a51f
gaoxiang <overgx_910@163.com>
2017-11-27 17:28:26 +0800

    格式化

--- src/main/java/com/takefree/configuration/DruidDataSourceConfiguration.java
+++ src/main/java/com/takefree/configuration/DruidDataSourceConfiguration.java
@@ -6,14 +6,16 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.alibaba.druid.pool.DruidDataSource;  
+import com.alibaba.druid.pool.DruidDataSource;
+
 @Configuration  
 public class DruidDataSourceConfiguration {
-  
-    @Bean  
-    @ConfigurationProperties(prefix = "spring.datasource")  
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource druidDataSource() {
-        DruidDataSource druidDataSource = new DruidDataSource();  
-        return druidDataSource;  
-    }  
-} 
+        DruidDataSource druidDataSource = new DruidDataSource();
+        return druidDataSource;
+    }
+}
+
