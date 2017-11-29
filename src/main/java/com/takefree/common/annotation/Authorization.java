1f4d3247429bc11ebd660fb24f851f25273b5efc
gaoxiang <overgx_910@163.com>
2017-11-29 13:45:20 +0800

    修改注释

--- src/main/java/com/takefree/common/annotation/Authorization.java
+++ src/main/java/com/takefree/common/annotation/Authorization.java
@@ -10,8 +10,7 @@ import java.lang.annotation.Target;
 
 /**
  * 在Controller的方法上使用此注解，该方法在映射时会检查用户是否登录，未登录返回401错误
- * @author ScienJus
- * @date 2015/7/31.
+ *
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
